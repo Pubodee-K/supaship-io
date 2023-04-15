53a80ea9596d34b9ef2d3d2d9224bae629d0d092
Pubodee (Grit) Kamonmasmatakul <grit@trienpont.com>
2023-04-16 03:54:36 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import NavBar from "./NavBar";
+import MessageBoard from "./MessageBoard";
+import AllPosts from "./AllPosts";
+import PostView from "./Post";
+import Welcome, {welcomeLoader} from "./Welcome";
+import { createContext } from "react";
+import { SupashipUserInfo, useSession } from "./use-session";
+
+const router = createBrowserRouter([
+  {
+    
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <MessageBoard />,
+        children: [
+          {
+            path: ":pageNumber",
+            element: <AllPosts />,
+          },
+          {
+            path: "post/:postId",
+            element: <PostView />,
+          },
+        ],
+      },
+      {
+        path: "welcome",
+        element: <Welcome />,
+        loader: welcomeLoader,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
+
+export const UserContext = createContext<SupashipUserInfo>({
+  session: null,
+  profile: null,
+});
+
+function Layout() {
+  const supashipUserInfo = useSession();
+  return (
+    <>
+      <UserContext.Provider value={supashipUserInfo}>
+        <NavBar />
+        <Outlet />
+      </UserContext.Provider>
+    </>
+  );
+}
